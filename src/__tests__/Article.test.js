4fd0b8d05df8307c54662e41e0d5dc39b066ca48
Adrian <adrianvalverde45@gmail.com>
2022-11-22 14:29:22 -0500

    commit tree fca993f7651f641919c94ae698bbbeec039e8835

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,7 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  const element = screen.queryByText("please pass this test")
+  
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
